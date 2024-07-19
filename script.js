479303e0b4934f4ec047fd3a52ebb09655f5bdd0
nairod010 <dorianbl@DESKTOP-TCIB0EC>
2024-07-20 01:03:46 +0300

    kinda done, need to fix a bug on read button

--- script.js
+++ script.js
@@ -1,70 +1,124 @@
-const myLibrary = [];
+let myLibrary = [];
 
-function Book(title, author, pages, volumes) {
+function Book(title, author, pages,read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.volumes = volumes
+    this.read = read;
 };
 
+Book.prototype.isRead = function() {
+    if(this.read === false){
+        this.read = true
+        return this.read;
+    }else{
+        this.read = false;
+        return this.read
+    }
+};
 
-function addBookToLibrary(title,author,pages,volumes) {
-    const newBook = new Book(title,author,pages,volumes);
+//let test = new Book("test", "test", 11, false);
+
+function addBookToLibrary(title,author,pages,read) {
+    const newBook = new Book(title,author,pages,read);
     myLibrary.push(newBook);
 };
 
-const testBook = new Book("test", "test","1","1");
-myLibrary.push(testBook);
-
+//addBookToLibrary("test","test",11, false);
 
 const container = document.getElementsByClassName("main-container");
 const form = document.querySelector(".register")
 const storedBooks = document.getElementById("stored-books");
 
-function addElement(title,author,pages,volumes){
+function addElement(title,author,pages,isRead){
 
     const newBook = document.createElement("div");
     newBook.setAttribute("id", "new-book")
+    
+    const closeButton = document.createElement("button");
+    closeButton.setAttribute("class", "remove")
+    closeButton.textContent = "Remove";
+    closeButton.addEventListener("click", () =>{
+        storedBooks.removeChild(newBook);
+        myLibrary = myLibrary.filter(book => book.title !== newTitle.textContent) 
+        console.log(myLibrary);
+    });   
 
     const newTitle = document.createElement(`p`);
     newTitle.setAttribute("id","new-title")
+    newTitle.setAttribute("class", "item")
     newTitle.textContent = `${title}`
 
     const newAuthor =  document.createElement(`p`);
     newAuthor.setAttribute("id","new-author")
+    newAuthor.setAttribute("class","item")
     newAuthor.textContent = `${author}`
 
     const newPages = document.createElement(`p`);
     newPages.setAttribute("id", "new-pages")
+    newPages.setAttribute("class","item")
     newPages.textContent = `${pages}`
 
-    const newVolumes = document.createElement(`p`);
-    newVolumes.setAttribute("id", "new-volumes")
-    newVolumes.textContent = `${volumes}`
+    const newStatus = document.createElement(`button`)
+    newStatus.setAttribute("id", "new-status")
+    if(isRead === false){
+        newStatus.textContent = "Not Read";
+    }else{
+        newStatus.textContent = "Read";
+    }
+
+    newStatus.addEventListener("click", () => {
+        getBook = myLibrary.filter(book => book.title === newTitle.textContent) 
+        console.log(getBook[0]);
+        getBook[0].isRead();
+        console.log(getBook);
+        if(getBook[0].read == true){
+            newStatus.textContent = "Read";
+            newStatus.style = "background-color: #efffc8; color: black; font-size: 1rem; font-weight: 700;border-color: #3b341f";
+            newStatus.addEventListener("mouseover", () => {
+                newStatus.style = "background-color: #85cb33;"
+            })
+        }else{
+            newStatus.textContent = "Not Read";
+            newStatus.style = "";
+        }
+    })
 
     newBook.appendChild(newTitle);
     newBook.appendChild(newAuthor);
     newBook.appendChild(newPages);
-    newBook.appendChild(newVolumes);
+    newBook.appendChild(newStatus);
+    newBook.appendChild(closeButton);
  
     storedBooks.appendChild(newBook); 
+
 };
 
 
 const title = document.getElementById("title");
 const author = document.getElementById("author");
 const pages = document.getElementById("pages");
-const volumes = document.getElementById("volumes");
+const stats = document.getElementById("toggle");
+
 
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    if(title.value !== "" || author.value !== "" || pages.value !== "" || volumes.value !== "" ){
-        addBookToLibrary(title.value, author.value, pages.value, volumes.value);
+    if(title.value !== "" || author.value !== "" || pages.value !== ""){ 
+        addBookToLibrary(title.value, author.value, pages.value, stats.checked);
+        addElement(title.value, author.value, pages.value, stats.checked);
     };
-    addElement(title.value, author.value, pages.value, volumes.value);
+
     console.log(myLibrary);
+
+    console.log(stats.checked);
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+
+
+
 })
 
 
@@ -73,13 +127,12 @@ const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("dialog + button");
 const closeButton = document.querySelector("dialog button");
 
-// "Show the dialog" button opens the dialog modally
 showButton.addEventListener("click", () => {
   dialog.showModal();
 });
 
-// "Close" button closes the dialog
 closeButton.addEventListener("click", () => {
   dialog.close();
 });
- 
+
+
